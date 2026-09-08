5e72d0fd9e03a0b24939f448ace6a19e12eddab5
jose <jose.topete0196@alumnos.udg.mx>
2026-09-08 16:46:59 -0600

    se reciben datos de manera correcta

--- recibir.php
+++ recibir.php
@@ -7,5 +7,19 @@
 </head>
 <body>
     <h1>Se guardaron las respuestas</h1>
-</body>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] = "POST"){
+    $nombre = $_POST['nombre'];
+     $correo = $_POST['correo'];
+      $fecha_de_nacimiento = $_POST['fecha_de_nacimiento'];
+
+      echo "<h2>Datos recibidos:</h2>";
+      echo "<p>Nombre: " . $nombre . "</p>";
+      echo "<p>Correo: " . $correo . "</p>";
+      echo "<p>Fecha de nacimiento: " . $fecha_de_nacimiento . "</p>";
+    }else{
+        echo "<p>No es una peticion tipo POST.</P>";
+ }
+ ?>
+</body> 
 </html>
